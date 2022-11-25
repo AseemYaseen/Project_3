3adfc29e89c29ba52b808a96a42c663c6d83ef8f
Asem <asemyassen300@gmail.com>
2022-11-25 21:17:46 +0300

    Test

---
+++ Testing1/ProjectEasy.js
@@ -0,0 +1,24 @@
+//Customer name = send value to local onchange
+// your order = send value to local onchange
+// Ask for cheef = read data from local to table
+// remove table content = delete the table (hide it)
+// delete local storage = clear data from storage
+
+let obj = {
+    
+}
+
+let arr = []
+
+function customerName() {
+   y =  document.getElementById('custName').value
+   console.log(y)
+    
+    
+}
+
+
+// let x = localStorage.setItem('Name', y)
+// console.log(customerName())
+
+// arr.push(x)
