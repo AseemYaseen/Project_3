907ede4d66d227a3be3107a60ea1a8d394046a4f
Asem <asemyassen300@gmail.com>
2022-11-26 21:23:30 +0300

    Test

--- Testing1/ProjectEasy.js
+++ Testing1/ProjectEasy.js
@@ -4,21 +4,60 @@
 // remove table content = delete the table (hide it)
 // delete local storage = clear data from storage
 
-let obj = {
-    
+// localStorage.clear()
+
+
+
+
+let eve = document.getElementById('AskFood').addEventListener('click',submitt)
+
+ document.getElementById('custName').value
+ document.getElementById('selcetFood').value
+
+let arrr;
+
+if (localStorage.pro != null) {
+    arrr = JSON.parse(localStorage.pro)
+}else {
+    arrr = []
 }
 
-let arr = []
 
-function customerName() {
-   y =  document.getElementById('custName').value
-   console.log(y)
+function submitt(e) {
+        e.preventDefault()
+        
+let price
+
+    if (selcetFood.value == 'Burger') {
+         price = 4
+    }
     
+    if (selcetFood.value == 'HotDogs') {
+         price = 3
+    }
     
+    if (selcetFood.value == 'Pizza') {
+         price = 7
+    }
+
+    let obj = {
+        name:custName.value,
+        food:selcetFood.value,
+        price:price
+    }
+    
+   arrr.push(obj)
+   localStorage.setItem('pro',JSON.stringify(arrr))
+
+   let y = JSON.parse(localStorage.getItem('pro'))
+   document.getElementById('p1').innerHTML = y
 }
 
 
-// let x = localStorage.setItem('Name', y)
-// console.log(customerName())
 
-// arr.push(x)
+
+
+
+
+
+
